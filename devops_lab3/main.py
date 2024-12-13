9fc52fff11624156636c662f293bf95af1956d47
plida <85080905+plida@users.noreply.github.com>
2024-12-13 18:17:23 +0300

    Update main.py

--- devops_lab3/main.py
+++ devops_lab3/main.py
@@ -1,6 +1,27 @@
-def greeting(name):
-      print(f"Hello, {name}!")
+def calculate(a, b, oper):
+      try:
+            a = int(a)
+            b = int(b)
+            if oper == "+":
+                  return a + b
+            if oper == "-":
+                  return a - b
+            if oper == "*":
+                  return a * b
+            if oper == "/" and b != 0:
+                  return a / b
+            return False
+      except TypeError:
+            print("TypeError!")
+            return False
+
 
 if __name__ == "__main__":
-      name = input("Input your name: ")
-      greeting(name)
+      a = input("Input a: ")
+      b = input("Input b: ")
+      oper = input("Input operation (+-*/): ")
+      res = calculate(a, b, oper)
+      if res:
+            print(f"Result {a} {oper} {b} = {calculate(a, b, oper)}")
+      else:
+            print("ERR")
