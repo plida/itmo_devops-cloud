a5eb3e338b29cbe434029a143a736909dbe57bbf
plida <85080905+plida@users.noreply.github.com>
2024-12-13 20:53:17 +0300

    Update test_main.py

--- devops_lab3/test_main.py
+++ devops_lab3/test_main.py
@@ -1,10 +1,7 @@
 import main
+import unittest
 
 def test_calculate():
-    assert main.calculate(2, 0, "/") == False
-    assert main.calculate(5, 2, "*") == 10
-    assert main.calculate(90, 3, "/") == 30
-    assert main.calculate(1, 0, "+") == 1
     assert main.calculate(100, 60, "-") == 40
 
 if __name__ == '__main__':
