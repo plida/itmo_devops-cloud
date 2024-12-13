ce72174abe62bcd157527411235e3911d17ea28b
plida <85080905+plida@users.noreply.github.com>
2024-12-13 21:03:00 +0300

    Update test_main.py

--- devops_lab3/test_main.py
+++ devops_lab3/test_main.py
@@ -1,9 +1,4 @@
 import main
-import unittest
 
-class TestCalc(unitest.TestCase):
-    def test_calculate(self):
-        self.assertEqual(main.calculate(100, 60, "-"), 40)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_calculate(self):
+    assert main.calculate(100, 60, "-") == 40
